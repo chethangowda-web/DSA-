1bce64852c90fde2fb00e43163b9dfd18bf5c94a
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-04-09 22:04:59 +0530

    Add files via upload

---
+++ DSA/LargestRectangleArea.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+       Stack<Integer> st = new Stack<>();
+       int maxArea = 0;
+       int n = heights.length;
+       for(int i = 0; i <= n; i++){
+        while(!st.isEmpty() && (i == n || heights[st.peek()] >= (i < n ? heights[i] : 0 ))){
+            int height = heights[st.pop()];
+            int width;
+
+            if(st.isEmpty()){
+                width = i;
+            }else{
+                width = i - st.peek() - 1;
+            }
+            maxArea = Math.max(maxArea , height * width);
+        }
+        st.push(i);
+       }
+    return maxArea;
+    }
+}
