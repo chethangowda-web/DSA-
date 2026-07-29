aaad38a9658a1bc4ad764803ac5914f2ac66af9c
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-07-29 11:24:13 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (45.49%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+
+        int l = 0;
+        int r = n - 1;
+
+        int lmax = 0;
+        int rmax = 0;
+
+        int water = 0;
+
+        while (l <= r) {
+
+            if (height[l] <= height[r]) {
+
+                if (height[l] >= lmax) {
+                    lmax = height[l];
+                } else {
+                    water += lmax - height[l];
+                }
+
+                l++;
+
+            } else {
+
+                if (height[r] >= rmax) {
+                    rmax = height[r];
+                } else {
+                    water += rmax - height[r];
+                }
+
+                r--;
+            }
+        }
+
+        return water;
+    }
+}
