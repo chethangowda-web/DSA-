216792dc60730ffdcfb70030ec180712e8483e66
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-07-29 07:29:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (43.95%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+       // 0 - red  1- white  2 - blue
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums , low , mid);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums , mid , high);
+                high--;
+            }
+        }
+    }
+
+    private void swap(int[] nums , int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
