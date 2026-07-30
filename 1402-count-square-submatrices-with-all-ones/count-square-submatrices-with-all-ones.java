92b89d416bdb1371d84a8aefb1818f8faefe743c
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-07-30 09:30:07 +0530

    Time: 8 ms (18.62%) | Memory: 61.7 MB (66.96%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -2,35 +2,22 @@ class Solution {
     public int countSquares(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] dp = new int[m][n];
-        for(int[] row : dp)
-            Arrays.fill(row , - 1);
+        int[][] dp = new int[m + 1][n + 1];
         int count = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                count += solve(i  , j , matrix , dp);
+        for(int i = m -1 ; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                int right = dp[i][j + 1];
+                int down = dp[i + 1][j];
+                int diagonal = dp[i+ 1][j + 1];
+                if(matrix[i][j] == 1){
+                    dp[i][j] = 1 + Math.min(right , Math.min(down , diagonal));
+                }else{
+                    dp[i][j] = 0;
+                }
+                  count += dp[i][j];
             }
         }
-            return count;
-        }
-    private int solve(int i , int j  , int[][]matrix , int[][]dp){
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        if(i >= m || j >= n)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-    
-
-        int right = solve(i  , j + 1 , matrix , dp);
-        int down = solve(i + 1, j , matrix , dp);    
-        int diagonal = solve(i + 1 , j + 1 , matrix ,dp);
-
-        if(matrix[i][j] == 0)
-            return dp[i][j] = 0;
-
-        return dp[i][j] = 1 + Math.min(right , Math.min(down , diagonal));
-
+        return count;
         }
 }
+    
