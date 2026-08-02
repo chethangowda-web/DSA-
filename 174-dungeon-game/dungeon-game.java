208856b958d2c20e12f3703863166569cdd48d69
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-08-02 09:43:18 +0530

    Time: 2 ms (73.69%) | Memory: 45.1 MB (73.49%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,25 +1,23 @@
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
-        // Reverse DP
-        // minimum health to save pricess
         int m = dungeon.length;
         int n = dungeon[0].length;
 
         int[][] dp = new int[m + 1][n + 1];
-        
-        for(int i = m - 1; i >= 0; i--){
-            for(int j = n -1 ; j >= 0; j--){
-                if(i == m - 1 && j == n - 1)
-                    dp[i][j] = Math.min(0 , dungeon[i][j]);
 
-                else if(i == m - 1)
-                    dp[i][j] = Math.min(0 , dungeon[i][j] + dp[i][j + 1]);
-                else if(j == n - 1)
-                    dp[i][j] = Math.min(0 , dungeon[i][j] + dp[i + 1][j]);
-                else
-                    dp[i][j] = Math.min(0 , dungeon[i][j] + Math.max(dp[i][j + 1] , dp[i + 1][j]));
+        for (int[] row : dp)
+            java.util.Arrays.fill(row, Integer.MAX_VALUE);
+
+        dp[m][n - 1] = 1;
+        dp[m - 1][n] = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int need = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+                dp[i][j] = Math.max(1, need);
             }
         }
-        return Math.abs(dp[0][0]) + 1;
-        }
+
+        return dp[0][0];
     }
+}
