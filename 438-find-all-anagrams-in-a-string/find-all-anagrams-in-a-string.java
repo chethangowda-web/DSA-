9f092f64a0098c2c2a33adec034f5e82da5f06c9
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-08-13 06:19:23 +0530

    Time: 13 ms (43.64%) | Memory: 46.5 MB (96.53%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -5,25 +5,29 @@ class Solution {
         if(s.length() < p.length()){
             return result;
         }
-        int[] freq = new int[26];
+
+        int[] Freq = new int[26];
         int[] windowFreq = new int[26];
 
         for(char c : p.toCharArray()){
-            freq[c - 'a']++;
+            Freq[c - 'a']++;
         }
+
         int left = 0;
         for(int right = 0; right < s.length(); right++){
             windowFreq[s.charAt(right) - 'a']++;
         
+
         if(right - left + 1 > p.length()){
             windowFreq[s.charAt(left) - 'a']--;
             left++;
         }
 
-        if(right - left + 1 == p.length() && Arrays.equals(freq , windowFreq)){
+        if(right - left + 1 == p.length() && Arrays.equals(Freq , windowFreq)){
             result.add(left);
         }
         }
         return result;
+        
     }
 }
