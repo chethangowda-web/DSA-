4340e9cb2c70fd6082ff4a40f64d6ff80a1ebfe8
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-09-10 12:00:47 +0530

    Time: 33 ms (70.92%) | Memory: 59.5 MB (20.79%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,41 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-    Arrays.sort(nums);
-    int n = nums.length;
-    List<List<Integer>> ans = new ArrayList<>();
-
-    for (int i = 0; i < n - 2; i++) {
-
-   if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-    }
-    int left = i + 1;
-    int right = n - 1;
-
-    while (left < right) {
-
-        int sum = nums[i] + nums[left] + nums[right];
-
-        if (sum == 0) {
-            ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-            left++;
-            right--;
-            while(left < right && nums[left] == nums[left - 1]){
-                left++;
-            }
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
         
+        for(int i = 0; i < nums.length - 2; i++){
+            int left = i + 1;
+            int right = nums.length - 1;
 
-            while(left < right && nums[right] == nums[right + 1]){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+
+            while(left < right){
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(sum == 0){
+                list.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+
+                while(left < right && nums[left] == nums[left + 1]){
+                    left++;
+                }
+
+                while(left < right && nums[right] == nums[right - 1]){
+                    right--;
+                }
+
+                left++;
                 right--;
             }
-        }else if (sum < 0) {
-            left++;
-        }
-        else {
-            right--;
+            else if(sum < 0){
+                left++;
+            }else{
+                right--;
+            }
         }
     }
-}
-return ans;
+        return list;
     }
 }
