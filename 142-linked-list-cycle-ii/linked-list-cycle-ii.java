3892aa9e19abb3db67d798bed086173fed76be68
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-09-23 22:16:37 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (85.95%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,16 +11,28 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Set<ListNode> visited = new HashSet<>();
 
-        while (head != null) {
-        if (visited.contains(head)) {
-        return head;
-    }
+        ListNode slow = head;
+        ListNode fast = head;
 
-    visited.add(head);
-    head = head.next;
-}
-    return null;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                slow = head;
+
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+
+                return slow;
+
+            }
+        }
+
+            return null;
     }
 }
+
